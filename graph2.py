edbd965a4edc2f7e9cc2e48ea9186c7744be000b
Aayuayushi <47912364+Aayuayushi@users.noreply.github.com>
2019-03-27 12:18:31 +0530

    Add files via upload

---
+++ graph2.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import csv
+
+x = []
+y = []
+
+with open('graph.csv','r') as csvfile:
+    plots = csv.reader(csvfile, delimiter=',')
+    for row in plots:
+        x.append(int(row[0]))
+        y.append(int(row[1]))
+
+plt.plot(x,y)
+plt.show()
